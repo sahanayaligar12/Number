2c1aec48d4ba3141f36a51f7d42f395dd69c9f5a
madhubevoor2004 <bevoormadhu@gmail.com>
2025-06-10 23:07:10 +0530

    Saved

---
+++ Even.java
@@ -0,0 +1,10 @@
+package Exam;
+
+public class Even {
+    public static void main(String[] args) {
+        int n=10;
+        if(n>0){
+            System.out.println("positive");
+        }
+    }
+}

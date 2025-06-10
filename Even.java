232a999315b04ae33cfc569b520eecf6755a0426
madhubevoor2004 <bevoormadhu@gmail.com>
2025-06-10 23:12:13 +0530

    updated

--- Even.java
+++ Even.java
@@ -6,5 +6,11 @@ public class Even {
         if(n>0){
             System.out.println("positive");
         }
+        else if(n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
     }
 }
